ea295022d4a71378244856097b1735f16a889953
Thyago Casablancas <thyago_casablancas@hotmail.com>
2023-08-10 16:44:14 -0300

    Changing button configuration

--- lib/components/buttons/plain_button.dart
+++ lib/components/buttons/plain_button.dart
@@ -3,31 +3,38 @@ library plain_button;
 import 'package:flutter/material.dart';
 
 class PlainButton extends StatefulWidget {
+  final String title;
+  final void buttonTap;
+
+  const PlainButton({
+    required Key key,
+    required this.title,
+    required this.buttonTap,
+  }) : super(key: key);
+
   @override
   _PlainButtonState createState() => _PlainButtonState();
 }
 
 class _PlainButtonState extends State<PlainButton> {
-  String title = "";
-
-  void changeTitle() {
-    setState(() {
-      title = "Mudado...";
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
-      height: 50,
-      child: TextButton(
-          onPressed: changeTitle,
-          style: TextButton.styleFrom(backgroundColor: Colors.blue),
-          child: const Text(
-            "Mudar",
-            style: TextStyle(color: Colors.white),
-          )),
-    );
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            shape: MaterialStatePropertyAll(
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+          ),
+          onPressed: () {
+            widget.buttonTap;
+          },
+          child: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ));
   }
 }
