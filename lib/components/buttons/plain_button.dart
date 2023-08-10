acf0f36b0b55a58dc1d5b7e82c91214ca3e2f440
Thyago Casablancas <thyago_casablancas@hotmail.com>
2023-08-10 14:23:21 -0300

    Update on button class

--- lib/components/buttons/plain_button.dart
+++ lib/components/buttons/plain_button.dart
@@ -3,7 +3,10 @@ library plain_button;
 import 'package:flutter/material.dart';
 
 class PlainButton extends StatefulWidget {
+  const PlainButton({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _PlainButtonState createState() => _PlainButtonState();
 }
 
@@ -20,8 +23,7 @@ class _PlainButtonState extends State<PlainButton> {
   Widget build(BuildContext context) {
     return TextButton(
         onPressed: changeTitle,
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey)),
+        style: TextButton.styleFrom(minimumSize: const Size.fromHeight(50)),
         child: Text("Inicial"));
   }
 }
