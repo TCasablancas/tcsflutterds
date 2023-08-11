98609cf71715674578a37bcfc21ec6d07a4ca9f5
Thyago Casablancas <thyago_casablancas@hotmail.com>
2023-08-11 16:26:48 -0300

    right action button app bar

--- lib/main_app_bar.dart
+++ lib/main_app_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tcsflutterds/components/labels/app_bar_label.dart';
 
-class MainAppBar extends StatelessWidget {
+class MainAppBar extends StatefulWidget {
   final String title;
 
   const MainAppBar({
@@ -10,11 +10,28 @@ class MainAppBar extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _MainAppBar createState() => _MainAppBar();
+}
+
+class _MainAppBar extends State<MainAppBar> {
+  @override
   Widget build(BuildContext context) {
     return AppBar(
       foregroundColor: Colors.black,
       centerTitle: false,
-      title: AppBarLabel(title: title),
+      title: AppBarLabel(title: widget.title),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: GestureDetector(
+            onTap: () {},
+            child: const Icon(
+              Icons.search,
+              size: 26.0,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
