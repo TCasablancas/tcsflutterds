ba073cce6a71a1d3908c4afef4612a714ef9c097
Thyago Casablancas <thyago_casablancas@hotmail.com>
2023-08-11 17:09:39 -0300

    setup navigation bar

--- lib/navigation_app_bar.dart
+++ lib/navigation_app_bar.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:tcsflutterds/components/labels/app_bar_label.dart';
 
-class MainAppBar extends StatefulWidget {
+class NavigationAppBar extends StatefulWidget {
   final String title;
+  final bool? isNavigation;
   final Icon? rightIcon;
+  final Icon? leadingIcon;
   final Function? rightAction;
+  final Function? leadingAction;
 
-  const MainAppBar({
+  const NavigationAppBar({
     Key? key,
     required this.title,
+    this.isNavigation,
     this.rightAction,
     this.rightIcon,
+    this.leadingIcon,
+    this.leadingAction,
   }) : super(key: key);
 
   @override
-  _MainAppBar createState() => _MainAppBar();
+  _NavigationAppBar createState() => _NavigationAppBar();
 }
 
-class _MainAppBar extends State<MainAppBar> {
+class _NavigationAppBar extends State<NavigationAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
       foregroundColor: Colors.black,
       centerTitle: false,
       title: AppBarLabel(title: widget.title),
+      leading: GestureDetector(
+        onTap: () => widget.leadingAction,
+        child: widget.leadingIcon,
+      ),
       actions: <Widget>[
         Padding(
           padding: const EdgeInsets.only(right: 20),
