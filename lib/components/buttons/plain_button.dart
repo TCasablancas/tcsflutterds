a56ba86b29b7dbc581245917736e672cd6d979a3
Thyago Casablancas <thyago_casablancas@hotmail.com>
2023-08-11 15:47:44 -0300

    Adjust font files

--- lib/components/buttons/plain_button.dart
+++ lib/components/buttons/plain_button.dart
@@ -1,6 +1,7 @@
 library plain_button;
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PlainButton extends StatefulWidget {
   final String title;
@@ -45,11 +46,9 @@ class _PlainButtonState extends State<PlainButton> {
               },
               child: Text(
                 widget.title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w700,
-                ),
+                style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w700,
+                    textStyle: const TextStyle(color: Colors.white)),
               ),
             )));
   }
