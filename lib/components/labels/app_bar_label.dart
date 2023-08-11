1188092cc1c4303fdf4b6fd95e8c2e836ec1d048
Thyago Casablancas <thyago_casablancas@hotmail.com>
2023-08-11 16:13:15 -0300

    create app bar

---
+++ lib/components/labels/app_bar_label.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppBarLabel extends StatelessWidget {
+  final String title;
+
+  const AppBarLabel({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        fontWeight: FontWeight.w700,
+        fontSize: 28,
+      ),
+    );
+  }
+}
---
+++ lib/main_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tcsflutterds/components/labels/app_bar_label.dart';
+
+class MainAppBar extends StatelessWidget {
+  final String title;
+
+  const MainAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        foregroundColor: Colors.black,
+        centerTitle: false,
+        title: AppBarLabel(title: title));
+  }
+}
