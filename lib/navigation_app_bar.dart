ef956f01821270de54bfbca4d3c62f0fc71b269f
Thyago Casablancas <thyago_casablancas@hotmail.com>
2023-08-11 17:11:13 -0300

    update navigation bar

--- lib/navigation_app_bar.dart
+++ lib/navigation_app_bar.dart
@@ -3,18 +3,12 @@ import 'package:tcsflutterds/components/labels/app_bar_label.dart';
 
 class NavigationAppBar extends StatefulWidget {
   final String title;
-  final bool? isNavigation;
-  final Icon? rightIcon;
   final Icon? leadingIcon;
-  final Function? rightAction;
   final Function? leadingAction;
 
   const NavigationAppBar({
     Key? key,
     required this.title,
-    this.isNavigation,
-    this.rightAction,
-    this.rightIcon,
     this.leadingIcon,
     this.leadingAction,
   }) : super(key: key);
@@ -34,15 +28,6 @@ class _NavigationAppBar extends State<NavigationAppBar> {
         onTap: () => widget.leadingAction,
         child: widget.leadingIcon,
       ),
-      actions: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(right: 20),
-          child: GestureDetector(
-            onTap: () => widget.rightAction,
-            child: widget.rightIcon,
-          ),
-        ),
-      ],
     );
   }
 }
